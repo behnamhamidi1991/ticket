a8ba5b9a59d5d4701d226fbd0c90db9c4a0c87c1
behnamhamidi1991 <eslp91@hotmail.com>
2024-03-01 06:02:57 +0330

    Commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const PORT = 8000;
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello');
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
