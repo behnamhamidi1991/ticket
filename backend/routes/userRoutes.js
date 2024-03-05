52bddbef7b8b937d005947232c6f5cb86df38576
behnamhamidi1991 <eslp91@hotmail.com>
2024-03-05 12:28:43 +0330

    Commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { loginUser, registerUser } = require("../controllers/userController");
 
-router.post("/", (req, res) => {
-  res.send("Register Route");
-});
+router.post("/", registerUser);
+
+router.post("/login", loginUser);
 
 module.exports = router;
