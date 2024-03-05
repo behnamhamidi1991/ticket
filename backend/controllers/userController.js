a60896ae566e1ed595d010e20ea2b361297c5fa2
behnamhamidi1991 <eslp91@hotmail.com>
2024-03-05 13:12:25 +0330

    DB connected

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Register a new user
 // @route /api/users
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   //   Validation
@@ -9,14 +11,14 @@ const registerUser = (req, res) => {
     res.status(400);
     throw new Error("Please include all fields");
   }
-};
+});
 
 // @desc Login a user
 // @route /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route");
-};
+});
 
 module.exports = {
   registerUser,
